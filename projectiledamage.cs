a961c592e1a20423f0531edf7bd8a22c8bacf070
fitoski <129151528+fitoski@users.noreply.github.com>
2023-04-28 14:32:33 +0300

    Update projectiledamage.cs

--- projectiledamage.cs
+++ projectiledamage.cs
@@ -2,22 +2,20 @@ using UnityEngine;
 
 public class ProjectileDamage : MonoBehaviour
 {
-    public int damage = 1;
-    private float lastDamageTime;
-    public float damageCooldown = 0.2f;
+    [SerializeField]
+    private int damage = 1;
 
-    private void OnTriggerEnter(Collider other)
+    void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Enemy"))
+        if (other.gameObject.CompareTag("Enemy"))
         {
-            if (Time.time > lastDamageTime + damageCooldown)
+            Health health = other.gameObject.GetComponent<Health>();
+            if (health != null)
             {
-                Health enemyHealth = other.GetComponent<Health>();
-                enemyHealth.TakeDamage(damage, gameObject);
-                lastDamageTime = Time.time;
-                Destroy(gameObject); 
+                health.TakeDamage(damage, gameObject); 
             }
         }
     }
+
 
 }
